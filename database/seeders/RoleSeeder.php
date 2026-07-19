49d43ba85e4f2c3bd1da0fea45d0ebf964d39c8b
mohammed <mohammedabuamsha8@gamil.com>
2026-07-19 16:24:14 +0300

    edit

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +14,10 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // php artisan db:seed --class=UserSeeder
+        DB::table('roles')->insert([
+            'id' => 1,
+            'role_name' => 'ادمن',
+        ]);
     }
 }
