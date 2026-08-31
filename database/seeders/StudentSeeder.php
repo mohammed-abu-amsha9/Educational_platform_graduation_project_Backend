62e1389b8b0f12768f05eddbb4a5aba97f9bbc7e
mohammed-abu-amsha9 <mohammedabuamsha8@gmail.com>
2026-08-31 11:06:18 +0300

    Update StudentSeeder.php

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Student;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -12,6 +13,17 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Student::create([
+            'id' => 1,
+            'full_name' => 'أحمد محمد علي أبو عيشة',
+            'student_code' => 'STU-2026-001',
+            'grade_id' => 1,
+            'section_id' => 1,
+            'total_paid_amount' => '500',
+            'parent_id' => '901234567',
+            'parent_phone' => '0591234567',
+            'parent_backup_phone' => '0561234567',
+            'account_status' => 'active',
+        ]);
     }
 }
