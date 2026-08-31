103fbccfa750a9bc59453801c3935369cf837467
mohammed-abu-amsha9 <mohammedabuamsha8@gmail.com>
2026-08-31 11:06:51 +0300

    Update TeacherSeeder.php

--- database/seeders/TeacherSeeder.php
+++ database/seeders/TeacherSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Teacher;
 
 class TeacherSeeder extends Seeder
 {
@@ -12,6 +13,13 @@ class TeacherSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Teacher::create([
+            'id' => 1,
+            'full_name' => 'محمود خالد عبد الله',
+            'teacher_code' => 'TCH-2026-001',
+            'phone_number' => '0599876543',
+            'account_status' => 'active',
+            'role_id' => 2,
+        ]);
     }
 }
